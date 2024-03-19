20fad2443b7051184f3e897ad496c08c4fb0f30c
mfglr <mfgglr@outlook.com>
2024-03-19 21:17:46 +0300

    Hosting ASP.NET Core images with Docker over HTTPS

---
+++ AspNetCoreDocker.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreDocker.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Merhaba dunya";
+        }
+    }
+}
